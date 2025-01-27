b19b01f5ef78a175ca0b37d89d39caa94d149289
Ipineda <irvin.pineda024@gmail.com>
2025-01-27 15:02:30 -0600

    New test

--- scripts.js
+++ scripts.js
@@ -101,7 +101,7 @@ function submitExam() {
 
     // Esperar 5 segundos y redirigir
     setTimeout(() => {
-        window.location.href = "https://teams.microsoft.com/l/entity/66aeee93-507d-479a-a3ef-8f494af43945/classroom?context=%7B%22subEntityId%22%3A%22%7B%5C%22version%5C%22%3A%5C%221.0%5C%22%2C%5C%22config%5C%22%3A%7B%5C%22classes%5C%22%3A%5B%7B%5C%22id%5C%22%3A%5C%22300f4adc-0573-4884-a31b-c21463df5f74%5C%22%2C%5C%22assignmentIds%5C%22%3A%5B%5C%223f0ccf1c-8674-4cbf-b414-24b329cdef02%5C%22%5D%7D%5D%7D%2C%5C%22action%5C%22%3A%5C%22navigate%5C%22%2C%5C%22view%5C%22%3A%5C%22assignment-viewer%5C%22%2C%5C%22appId%5C%22%3A%5C%225e3ce6c0-2b1f-4285-8d4b-75ee78787346%5C%22%2C%5C%22deeplinkType%5C%22%3A4%7D%22%2C%22channelId%22%3Anull%7D";
+        window.location.href = "https://teams.microsoft.com/l/message/19:vMeyOsex9uupiL7GvIHXusqdEq4rwRHfRDtRWUbTFOE1@thread.tacv2/1737993589970?tenantId=d8e3bd44-0bba-426b-952d-ada7bb17393c&groupId=1bf8e2cb-e994-414a-9c08-3f059c7e5a37&parentMessageId=1737993589970&teamName=ICT%20Second%20Year&channelName=General&createdTime=1737993589970";
     }, 5000);
 }
 
